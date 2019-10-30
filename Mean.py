e9df5024ad3b0655e53aec6b26232309e3abc643
Prajakta <prajaktavpendse@gmail.com>
2019-10-30 12:04:05 -0400

    Feature: Added Mean.py file

---
+++ Mean.py
@@ -0,0 +1,27 @@
+
+class mean:
+
+  print("mean")
+
+
+
+
+#avg=float(n/length)
+
+#print(avg)
+
+
+
+
+
+#data =[10,20,30,40]
+
+#data1=len(data)
+
+#mean =sum(data)/data1
+
+#print(mean)
+
+#tupleA=(1,2,3,4,5,)
+
+#print(statistics.mean(tupleA))
